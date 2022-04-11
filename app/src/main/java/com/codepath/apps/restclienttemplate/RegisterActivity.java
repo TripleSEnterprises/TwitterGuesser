68cfeb53ae2e0cfd87329155375f6167a94ecc3f
Felix Luciano <felixluciano.a@gmail.com>
2022-04-10 22:20:58 -0400

    Image upload from storage added. Missing database upload.

--- app/src/main/java/com/codepath/apps/restclienttemplate/RegisterActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/RegisterActivity.java
@@ -4,22 +4,29 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.switchmaterial.SwitchMaterial;
 import com.google.android.material.textfield.TextInputEditText;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
 import java.io.IOException;
 
 import okhttp3.Call;
@@ -34,6 +41,8 @@ public class RegisterActivity extends AppCompatActivity {
     SwitchMaterial swProfile;
     Button btnUpload;
     ImageView ivProfile;
+    Uri selectedImage;
+    String part_image;
 
     public static final String TAG = "RegisterActivity";
     @Override
@@ -53,16 +62,21 @@ public class RegisterActivity extends AppCompatActivity {
             ParseUser.getCurrentUser().saveInBackground(new SaveCallback() {
                 @Override
                 public void done(ParseException e) {
-                    Log.i(TAG, "User Registered as "+username);
-                    goToMainActivity();
+                    if (e==null)
+                        Log.i(TAG,"Saved username " + username);
+                    else
+                        Log.e(TAG,"Error saving username", e);
                 }
             });
             if(swProfile.isChecked()){
+                Log.i(TAG,"Using twitter image");
                 useTwitterImage();
             }
             else{
-                uploadImage();
+                Log.i(TAG,"Using uploaded image at "+part_image);
+//                uploadImage();
             }
+            goToMainActivity();
         });
 
         swProfile.setOnClickListener(v->{
@@ -76,6 +90,10 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
 
+        btnUpload.setOnClickListener(v -> {
+            getImage();
+        });
+
     }
 
     private void goToMainActivity() {
@@ -84,9 +102,62 @@ public class RegisterActivity extends AppCompatActivity {
 		startActivity(i);
     }
 
-    private void uploadImage(){
+    private void getImage(){
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/");
+        startActivityForResult(intent,100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode == 100 && resultCode == RESULT_OK){
+            selectedImage = data.getData();
+            String[] imageProjection = {MediaStore.Images.Media.DATA};
+            Cursor cursor = getContentResolver().query(selectedImage,imageProjection,null,null,null);
+            if(cursor != null){
+                cursor.moveToFirst();
+                int indexImage = cursor.getColumnIndex(imageProjection[0]);
+                part_image = cursor.getString(indexImage);
+                Bitmap bitmap = null;
+                try{
+                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),selectedImage);
+                }catch(IOException e){
+                    Log.e(TAG,"Error retrieving file",e);
+                }
+                Glide.with(this)
+                        .load(bitmap)
+                        .circleCrop()
+                        .into(ivProfile);
+            }
+        }
 
     }
+
+    private void uploadImage(){
+        File image = new File(part_image);
+        ParseFile parsefile = new ParseFile(image);
+        parsefile.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e!= null){
+                    Log.e(TAG,"File could not be saved",e);
+                    return;
+                }
+                ParseUser.getCurrentUser().put("picture",image);
+                ParseUser.getCurrentUser().saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e==null)
+                            Log.i(TAG,"Saved username and image");
+                        else
+                            Log.e(TAG,"Error saving user",e);
+                    }
+                });
+            }
+        });
+    }
+
     private void useTwitterImage(){
         Log.i(TAG,"Getting user Image");
         try{
@@ -108,8 +179,8 @@ public class RegisterActivity extends AppCompatActivity {
                         e.printStackTrace();
                     }
                     String finalImage = image;
-                    Log.i(TAG,image);
-                    ParseUser.getCurrentUser().put("cachedPicture",image);
+                    Log.i(TAG,finalImage);
+                    ParseUser.getCurrentUser().put("cachedPicture",finalImage);
                     ParseUser.getCurrentUser().saveInBackground(v->{
                         Log.i(TAG,"Image saved "+ finalImage);
                     });
