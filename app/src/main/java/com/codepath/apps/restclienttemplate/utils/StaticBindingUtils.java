132e2b8dd813943de0fbd9b4ba7a056b0fb17726
John Santelises <43687971+GamingDoge69@users.noreply.github.com>
2022-04-16 03:34:56 -0400

    Leaderboard Position Rendering

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/utils/StaticBindingUtils.java
@@ -0,0 +1,50 @@
+package com.codepath.apps.restclienttemplate.utils;
+
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.databinding.BindingAdapter;
+
+import com.codepath.apps.restclienttemplate.R;
+import com.parse.CountCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+public class StaticBindingUtils {
+    private static final String TAG = "StaticBindingUtils";
+
+    private static final String[] suffixes = new String[] { "th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th" };
+
+    @BindingAdapter("profilePositionFormatter")
+    public static void profilePositionFormatter(TextView textView, ParseUser user) {
+        ParseQuery<ParseUser> query = ParseQuery.getQuery(ParseUser.class)
+                .whereGreaterThan("highScore", user.getNumber("highScore"));
+        query.countInBackground(new CountCallback() {
+            @Override
+            public void done(int count, ParseException e) {
+                int position = count + 1;
+                if (e == null) {
+                    String suffix;
+                    switch (position % 100) {
+                        case 11:
+                        case 12:
+                        case 13:
+                            suffix = "th";
+                            break;
+                        default:
+                            suffix = suffixes[(int) (position % 10)];
+                    }
+                    textView.setText(textView.getContext().getString(
+                            R.string.profile_leaderboard_position,
+                            position,
+                            suffix)
+                    );
+                }
+                else {
+                    Log.wtf(TAG, e);
+                }
+            }
+        });
+    }
+}
