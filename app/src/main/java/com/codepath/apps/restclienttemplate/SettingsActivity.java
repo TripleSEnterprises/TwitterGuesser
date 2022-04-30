83e9b4ff51d50463ebf02c0f7763b2dddc08fee0
Felix Luciano Salomon <felixluciano.a@gmail.com>
2022-04-30 16:40:50 -0400

    Created layout for settings screen

--- app/src/main/java/com/codepath/apps/restclienttemplate/SettingsActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/SettingsActivity.java
@@ -1,14 +1,20 @@
 package com.codepath.apps.restclienttemplate;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.databinding.DataBindingUtil;
 import android.os.Bundle;
 
+import com.codepath.apps.restclienttemplate.databinding.ActivitySettingsBinding;
+
 public class SettingsActivity extends AppCompatActivity {
+
+    ActivitySettingsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_settings);
+
+
     }
 }
